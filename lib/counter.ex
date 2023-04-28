bf32bdb6822970b644ebbb8100bc1b318ed89c31
Musyimi <musyimi13@gmail.com>
2023-04-28 11:05:42 +0300

    basics on genserver functions and callbacks

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,3 +1,39 @@
 defmodule Counter do
+  use GenServer
 
+  def start_link(initial_count) do
+    GenServer.start_link(__MODULE__, initial_count)
+  end
+
+  def increment(pid) do
+    GenServer.call(pid, :inc)
+  end
+
+  def decrement(pid) do
+  GenServer.call(pid, :dec)
+  end
+
+  def current(pid) do
+    GenServer.call(pid, :current)
+  end
+
+  
+
+  def init(initial_count) do
+     {:ok, initial_count}
+  end
+
+  def handle_call(:inc, _from, count) do
+    updated_count = count + 1
+    {:reply, updated_count, updated_count}
+  end
+
+  def handle_call(:dec, _from, count) do
+    updated_count = count - 1
+    {:reply, updated_count, updated_count}
+  end
+
+  def handle_call(:current, _from, count) do
+    {:reply, count, count}
+  end
 end
